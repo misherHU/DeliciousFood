6852a8f4280fe999a71ebe60ed656be977c9cd7b
misher <misher.wedervoort@student.hu.nl>
2021-07-05 09:20:58 +0200

    sprint4 commit and push

--- src/test/java/org/example/DomeinTest.java
+++ src/test/java/org/example/DomeinTest.java
@@ -1,11 +1,12 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import IPASS.DomeinModellen.Klant;
 import IPASS.DomeinModellen.Recept;
 import IPASS.DomeinModellen.Review;
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.util.ArrayList;
@@ -34,20 +35,20 @@ public class DomeinTest{
     @Test
     public void gebruikersNaamMinderDan20Karakters() throws CloneNotSupportedException {
         Klant klant= new Klant("misher","meterman","man","mishernon@hotmail.com","wiwi",likes,"klant",new ArrayList<>());
-        assertTrue("Het gebruikersnaam is te lang", klant.getName().length()<20);
+        assertTrue(klant.getName().length()<20, "Het gebruikersnaam is te lang");
     }
     @Test
     public void wachtwoordMinderDan20Karakters() throws CloneNotSupportedException {
         Klant klant= new Klant("misher","meterman","man","mishernon@hotmail.com","wiwi",likes,"klant",new ArrayList<>());
 
-        assertTrue("Het wachtwoord is te lang", klant.getWachtwoord().length()<20);
+        assertTrue(klant.getWachtwoord().length()<20, "Het wachtwoord is te lang");
     }
     @Test
     public void reviewTekstMinderDan200Karakters()
     {
 
         Review review=new Review(klant.getName(),"grbhskugkersbuybkgresuybgkresuybgkrseyugrbeksyugrbsekyugbresyku");
-        assertTrue("Het review tekst is te lang", review.getTekst().length()<200);
+        assertTrue(review.getTekst().length()<200, "Het review tekst is te lang");
     }
 
 
