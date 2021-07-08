52d4fefeee91f2e4683ef4112ec12a68b9ef93f2
misher <misher.wedervoort@student.hu.nl>
2021-07-08 03:30:05 +0200

    sprint4 commit and push2

--- src/test/java/org/example/DomeinTest.java
+++ src/test/java/org/example/DomeinTest.java
@@ -1,31 +1,31 @@
 package org.example;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import IPASS.DomeinModellen.Klant;
 import IPASS.DomeinModellen.Recept;
 import IPASS.DomeinModellen.Review;
-import org.testng.annotations.Test;
-import org.junit.jupiter.api.BeforeEach;
 
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 /**
  * Unit test for simple App.
  */
 public class DomeinTest{
-    private ArrayList<Recept> likes;
-    private Klant klant;
-    private Recept recept;
+    private static ArrayList<Recept> likes;
+    private static Klant klant;
+    private static Recept recept;
 
-    @BeforeEach
-    public void initialize() throws CloneNotSupportedException {
+    @BeforeAll
+    static void initialize() throws CloneNotSupportedException {
         likes=new ArrayList<>();
         //recept=new Recept("batat","15 minuten","snacks","fgbgf");
 
         likes.add(recept);
-        klant= new Klant("misher","meterman","man","mishernon@hotmail.com","wiwi",likes,"klant",new ArrayList<>());
+        klant= new Klant("misher1","meterman","man","mishernon1@hotmail.com","wiwi",likes,"klant",new ArrayList<>());
 
 
     }
@@ -34,18 +34,19 @@ public class DomeinTest{
 
     @Test
     public void gebruikersNaamMinderDan20Karakters() throws CloneNotSupportedException {
-        Klant klant= new Klant("misher","meterman","man","mishernon@hotmail.com","wiwi",likes,"klant",new ArrayList<>());
+        Klant klant= new Klant("misher2","meterman","man","mishernon2@hotmail.com","wiwi",likes,"klant",new ArrayList<>());
         assertTrue(klant.getName().length()<20, "Het gebruikersnaam is te lang");
     }
     @Test
     public void wachtwoordMinderDan20Karakters() throws CloneNotSupportedException {
-        Klant klant= new Klant("misher","meterman","man","mishernon@hotmail.com","wiwi",likes,"klant",new ArrayList<>());
+        Klant klant= new Klant("misher3","meterman","man","mishernon3@hotmail.com","wiwi",likes,"klant",new ArrayList<>());
 
         assertTrue(klant.getWachtwoord().length()<20, "Het wachtwoord is te lang");
     }
     @Test
     public void reviewTekstMinderDan200Karakters()
     {
+        System.out.println(klant.getName());
 
         Review review=new Review(klant.getName(),"grbhskugkersbuybkgresuybgkresuybgkrseyugrbeksyugrbsekyugbresyku");
         assertTrue(review.getTekst().length()<200, "Het review tekst is te lang");
